92b1ad9a1396e4b24a9a6028a0e4fcbce73ced68
Apoorva Mundada <apoorva.aam@gmail.com>
2025-10-24 23:29:13 +0530

    feat: added themes

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[])
 {
+    QCoreApplication::setOrganizationName("DoonMadOrg");
+    QCoreApplication::setApplicationName("ShowerToys");
+
+
+    QApplication::setStyle(QStyleFactory::create("windowsvista"));
+
     QApplication a(argc, argv);
     a.setQuitOnLastWindowClosed(false);
 
